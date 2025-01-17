e5e967bb8a7a712763f0447a44e58a989a9396a9
Jayanta66 <jayhalder18@gmail.com>
2025-01-17 21:07:54 +0530

    Image Slider With Spring boot Thymeleaf

---
+++ src/main/java/com/example/imageSlider/ImageSliderApplication.java
@@ -0,0 +1,13 @@
+package com.example.imageSlider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageSliderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageSliderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/imageSlider/ImageSliderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.imageSlider;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageSliderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
